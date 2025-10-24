58d76e08fa4c5ec6ccf9fe45e74d3215f18ebe91
Lily Botha <lilybotha06@gmail.com>
2025-10-24 22:30:47 +0200

    Fixed small typos and naming conventions

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,21 +1,15 @@
 extends Control
 
 @onready var start: Button = $start
-@onready var controls: Button = $controls
-@onready var credits: Button = $credits
 @onready var squire: Label = $squire
 @onready var attendme: Label = $attendme
 
 var controls_pressed: bool = false
-var credits_pressed: bool = false
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass
 
-
-
-	# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
@@ -23,14 +17,10 @@ func _on_button_pressed() -> void:
 	start.queue_free()
 	squire.queue_free()
 	attendme.queue_free()
-	#add gamestate here
-
+	# Add gamestate here
 
 func _on_button_controls_pressed() -> void:
 	pass
 
-
-
 func _on_button_credits_pressed() -> void:
-	pass # Replace with function body.
-	
+	pass 
