7ea55495ffffff7d9e2aa68672073bcee993de09
KaiyoLyn <juliamreijen@gmail.com>
2025-10-25 19:17:15 +0200

    Autoscroller better

---
+++ scripts/scroll_controller.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const SPEED : int = 10
+
+# Called when the node enters the scene tree for the first time.
+
+var direction = Vector2(-1,0)
+
+@onready var backdrop: Parallax2D = $".."
+@onready var terrain: Parallax2D = $"../../Terrain"
+@onready var props: Parallax2D = $"../../Props"
+
+
+func _process(delta: float) -> void:
+    backdrop.scroll_offset += direction * SPEED * delta
+    terrain.scroll_offset += direction * SPEED * delta
+    props.scroll_offset += direction * SPEED * delta
--- scripts/world.gd
+++ scripts/world.gd
@@ -11,10 +11,10 @@ func _on_tree_entered() -> void:
     SignalBus.start_scrolling.connect(pause_scrolling)
     SignalBus.start_scrolling.connect(start_scrolling)
 
+#func _process(delta: float) -> void:
+ #   if camera_active:
+  #      camera.position.x += camera_speed * delta
 
-func _process(delta: float) -> void:
-    if camera_active:
-        camera.position.x += camera_speed * delta
 
 func start_scrolling() -> void:
     camera_active = true
