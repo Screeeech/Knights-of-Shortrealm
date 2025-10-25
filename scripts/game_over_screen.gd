548d2968342d103d2f58da7c8f216fe1b85edd43
KaiyoLyn <juliamreijen@gmail.com>
2025-10-25 20:54:51 +0200

    game over + stage pictures

--- scripts/game_over_screen.gd
+++ scripts/game_over_screen.gd
@@ -1,16 +1,28 @@
 extends Control
 
+@onready var game_over: Label = $GameOver
+@onready var text: Label = $Text
+@onready var restart: Button = $Restart
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass 
+    game_over.hide()
+    text.hide()
+    restart.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
      pass
 
-
+func _input(ev):
+    if Input.is_key_pressed(KEY_CTRL):
+        game_over.show()
+        text.show()
+        restart.show()
+        
 
 func _on_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/UI.tscn")
+    get_tree().change_scene_to_file("res://world.tscn")
--- scripts/scroll_controller.gd
+++ scripts/scroll_controller.gd
@@ -9,9 +9,12 @@ var direction = Vector2(-1,0)
 @onready var backdrop: Parallax2D = $".."
 @onready var terrain: Parallax2D = $"../../Terrain"
 @onready var props: Parallax2D = $"../../Props"
+@onready var foreground: Parallax2D = $"../../foreground"
+
 
 
 func _process(delta: float) -> void:
     backdrop.scroll_offset += direction * SPEED * delta
     terrain.scroll_offset += direction * SPEED * delta
     props.scroll_offset += direction * SPEED * delta
+    foreground.scroll_offset += direction * SPEED * delta
