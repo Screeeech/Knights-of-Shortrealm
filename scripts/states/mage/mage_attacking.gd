64e7be63b03f90f9ae9a733f880493cbcad159df
Lily Botha <lilybotha06@gmail.com>
2025-10-26 15:45:14 +0100

    Added fireball boilerplate

--- scripts/states/mage/mage_attacking.gd
+++ scripts/states/mage/mage_attacking.gd
@@ -6,8 +6,15 @@ extends State
 @export var escaping_state: State
 @export var exploofing_state: State
 
+var timer := Timer.new()
+
+func _ready() -> void:
+    add_child(timer)
+    timer.timeout.connect(_on_animation_finished)
+
 func enter() -> void:
     super()
+    timer.call_deferred("start", 1)
 
 func exit() -> void:
     pass
@@ -17,3 +24,6 @@ func process_physics(_delta: float) -> State:
 
 func process_frame(_delta: float) -> State:
     return null
+
+func _on_animation_finished() -> void:
+    pass
