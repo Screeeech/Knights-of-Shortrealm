d49884910cd87ab78805f097d57e3514f0a3d1bf
Lily Botha <lilybotha06@gmail.com>
2025-10-26 13:53:01 +0100

    Started exploof

---
+++ scripts/enemies/exploof.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var sprite: Sprite2D = $ExploofSprite
+@onready var animation: AnimationPlayer = $ExploofAnimation
+@onready var alive: bool = true
+
+func _ready() -> void:
+    pass
+
+func exploof() -> void:
+    sprite.show()
+    animation.play("exploof")
+    alive = false
+
+func _process(_delta: float) -> void:
+    if not alive and not animation.is_playing():
+        get_parent().queue_free()
+
